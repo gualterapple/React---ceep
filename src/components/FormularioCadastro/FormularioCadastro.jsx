a51af6123e483b98292643f1b9eb3d30ca5b09b2
gualterapple <39972000+gualterapple@users.noreply.github.com>
2021-09-14 06:54:55 +0100

    Módulo 2 do React concluído

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -7,6 +7,23 @@ class FormularioCadastro extends Component {
     super(props);
     this.titulo = "";
     this.texto = "";
+    this.categoria = "Sem Categoria";
+    this.state = {categorias: []}
+
+  }
+
+  componentDidMount()
+  {
+    this.props.categorias.inscrever(this._novasCategorias.bind(this));
+  }
+
+  componentWillUnmount(){
+    this.props.categorias.desinscrever(this._novasCategorias.bind(this));
+  }
+
+  _novasCategorias(categorias)
+  {
+    this.setState({...this.state, categorias});
   }
 
   _handleMudancaTitulo(evento)
@@ -18,17 +35,29 @@ class FormularioCadastro extends Component {
   {
     this.texto = evento.target.value;
   }
+  _handleChangeMudancaCategoria(evento)
+  {
+    this.categoria = evento.target.value;
+  }
 
   _criarCard(evento)
   {
     evento.preventDefault();
     evento.stopPropagation();
-    this.props.criarNota_prop(this.titulo, this.texto);
+    this.props.criarNota(this.titulo, this.texto, this.categoria);
   }
 
   render() {
     return (
       <form className="form-cadastro" onSubmit={this._criarCard.bind(this)}>
+        <select 
+        onChange={this._handleChangeMudancaCategoria.bind(this)}
+        className="form-cadastro_input">
+          <option>Sem Categoria</option>
+          {this.props.categorias.categorias.map((categoria, index) =>{
+              return <option key={index}>{categoria}</option>
+            })} 
+        </select>
         <input type="text" placeholder="Titulo" className="form-cadastro_input"
         onChange={this._handleMudancaTitulo.bind(this)}/>
         <textarea rows="15" placeholder="Escreva sua nota" className="form-cadastro_input"
---
+++ src/dados/Categorias.js
@@ -0,0 +1,31 @@
+export default class Categorias
+{
+    constructor()
+    {
+        this.categorias = [];
+        this._inscritos = [];
+    }
+
+    inscrever(func)
+    {
+      this._inscritos.push(func);
+    }
+
+    desisnscrever(func)
+    {
+      this._inscritos = this._inscritos.filter(f => f !== func);
+    }
+
+    notificar()
+    {
+      this._inscritos.forEach(func => {
+        func(this.categorias);
+      });
+    }
+
+    adicionarCategoria(categoria)
+    {
+      this.categorias.push(categoria);
+      this.notificar();
+    }
+}
